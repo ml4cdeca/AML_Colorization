213ccf5980b280ed25b7a1886d79e1c1b30826a7
chrislbie <christopher.lueken.winkels@gmail.com>
2019-08-28 15:57:26 +0200

    new approach for discriminator and first trail of implementation in train_gan

--- models/discriminator.py
+++ models/discriminator.py
@@ -19,6 +19,25 @@ class critic(nn.Module):
         x=self.fc(x)
         return self.sig(x)
 
+class markov_critic(nn.Module):
+    '''
+    input: grayscale (first 3 channels) and colored (last 3 channels) image
+
+    return: real/fake classification of "patches" of input as tensor
+    (generator loss will be cronstructed form mean)
+    '''
+    def __init__(self):
+        self.cnn = nn.Sequential(convBlock(6,16,kernel_size=2),
+                                convBlock(16,32,kernel_size=2),
+                                convBlock(32,64,kernel_size=2),
+                                convBlock(64,128,kernel_size=2),
+                                convBlock(128,1,kernel_size=1))
+        self.sig=nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.cnn(x)
+        return self.sig(x)
+
 class convBlock(nn.Module):
     def __init__(self,in_channels,out_channels,kernel_size=3,stride=2,padding=1):
         super(convBlock,self).__init__()
