c787239f8f81ee9474f6770f79f80e9df844d2ca
Lukas <luk.blecher@gmx.de>
2019-09-10 21:24:03 +0200

    general cifar path

--- train_classification.py
+++ train_classification.py
@@ -91,7 +91,7 @@ def main(argv):
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     dataset=None
     in_size = 256
-    if data_path == './cifar-10':
+    if 'cifar' in data_path:
         in_size = 32
         dataset = 0
     elif 'places' in data_path:
